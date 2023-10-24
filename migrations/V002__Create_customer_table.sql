f81fd46ccb0719a7625e6c7564e3f15ca981614c
Kunle Balogun <kunle@kunlebalogun.com>
2023-10-24 14:37:44 +0100

    Create V002__Create_customer_table.sql
    new table

---
+++ migrations/V002__Create_customer_table.sql
@@ -0,0 +1,9 @@
+-- Create the customer table
+CREATE TABLE customer.customer (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255),
+    phone_number VARCHAR(20),
+    address TEXT
+);
